22d8041a50a9f023485014cd2c7621a6e973f083
DarkExplorer31 <drkexplorer@gmal.com>
2024-01-27 11:37:56 +0100

    add performance tests with Locust

---
+++ tests/performance/locustfile.py
@@ -0,0 +1,35 @@
+from locust import HttpUser, task
+
+
+class ProjectPerfTest(HttpUser):
+    @task
+    def home(self):
+        self.client.get("/")
+
+    @task
+    def book_path(self):
+        self.client.get("/book/Fall Classic/She Lifts")
+
+    @task
+    def purchase(self):
+        response = self.client.post(
+            "/purchasePlaces",
+            {
+                "competition": "Spring Festival",
+                "club": "Simply Lift",
+                "places": 1,
+            },
+        )
+        if response.status_code == 400:
+            print(
+                "Received a 400 response code for the purchase task. It's because the club is unable to purchase places. Continuing..."
+            )
+
+    @task
+    def table(self):
+        self.client.get("/pointsDisplay")
+
+    @task
+    def login_and_logout(self):
+        self.client.post("/showSummary", {"email": "john@simplylift.co"})
+        self.client.get("/logout")
